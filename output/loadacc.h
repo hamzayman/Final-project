c7c87c7b05efe4b04c36da51a60eac6150753f4a
Hamza <hamza99ayman250@gmail.com>
2025-12-20 21:28:17 +0200

    features added : login / startmenu /load / menu(unfinished)

---
+++ output/loadacc.h
@@ -0,0 +1,21 @@
+#ifndef LOADACC_H
+#define LOADACC_H
+
+typedef struct {
+    int month;
+    int year;
+} date;
+
+typedef struct {
+    long long accountnumber;
+    char name[100];
+    char address[100];
+    double balance; 
+    long long mobile;
+    date dateopened;
+    char status[100];
+} account;
+
+void loadaccounts(void);
+
+#endif
